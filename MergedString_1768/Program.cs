3a6d453a7b23796f52e5af3c7e15c06af5560dab
Mureulos <murilodesantana05@gmail.com>
2025-07-20 19:46:09 -0300

    Adicionar arquivos de projeto.

---
+++ MergedString_1768/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace MergedString_1768;
+
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        List<char> merged = new List<char>();
+        int i = 0;
+        int j = 0;
+
+        while (i < word1.Length || j < word2.Length)
+        {
+            if (i < word1.Length)
+            {
+                merged.Add(word1[i]);
+                i++;
+            }
+
+            if (j < word2.Length)
+            {
+                merged.Add(word2[j]);
+                j++;
+            }
+        }
+
+        return new string(merged.ToArray());
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string word1 = "abc";
+        string word2 = "pqr";
+        string result = solution.MergeAlternately(word1, word2);
+        Console.WriteLine(result);
+    }
+}
